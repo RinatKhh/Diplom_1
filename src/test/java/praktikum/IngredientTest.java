73156c9edd77a831822f1de76e8551e63622d7bb
RinatKhh <fatalist@internet.ru>
2022-10-13 13:37:07 +0300

    ver 1.1 add parameter in IngredientTest

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,24 +1,46 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 import static praktikum.IngredientType.*;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
-    Ingredient ingredient = new Ingredient(SAUCE,"тест",1.0F);
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+    Ingredient ingredient;
+
+    public IngredientTest(IngredientType type, String name, float price, Ingredient ingredient) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+        this.ingredient = ingredient;
+    }
+
+    @Parameterized.Parameters // добавили аннотацию
+    public static Object[][] getData() {
+        return new Object[][] {
+                { SAUCE,"тест сoус",1.0F,new Ingredient(SAUCE,"тест сoус",1.0F)},
+                { FILLING,"тест начинка",1.0F,new Ingredient(FILLING,"тест начинка",1.0F)},
+        };
+    }
+
     @Test
     public void getPrice() {
-        assertEquals(1,ingredient.getPrice(),0);
+        assertEquals(price,ingredient.getPrice(),0);
     }
 
     @Test
     public void getName() {
-        assertEquals("тест",ingredient.getName());
+        assertEquals(name,ingredient.getName());
     }
 
     @Test
     public void getType() {
-        assertEquals(SAUCE,ingredient.getType());
+        assertEquals(type,ingredient.getType());
     }
 }
